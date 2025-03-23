b3073938716fe2e1446b1df2124c2242ad6053c7
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-23 09:15:51 -0500

    dto para el response de categoria

---
+++ src/main/java/com/isai/api_rest_buenas_practicas/models/dtos/CategoryResponse.java
@@ -0,0 +1,11 @@
+package com.isai.api_rest_buenas_practicas.models.dtos;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CategoryResponse {
+    private Long categoryId;
+    private String name;
+}
