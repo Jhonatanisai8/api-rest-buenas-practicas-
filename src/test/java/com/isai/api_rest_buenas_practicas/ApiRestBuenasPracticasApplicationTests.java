b91f4dee5013fb4ce86d6135d0827e7af89e2df2
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-23 08:39:59 -0500

    init

---
+++ src/test/java/com/isai/api_rest_buenas_practicas/ApiRestBuenasPracticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.api_rest_buenas_practicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBuenasPracticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
