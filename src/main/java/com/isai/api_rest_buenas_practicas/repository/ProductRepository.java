106297a23c2c521a43b136829127ee2cd308a828
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-23 08:57:40 -0500

    definicion de un metodo para listar todos los productos de una categoria

--- src/main/java/com/isai/api_rest_buenas_practicas/repository/ProductRepository.java
+++ src/main/java/com/isai/api_rest_buenas_practicas/repository/ProductRepository.java
@@ -1,12 +1,16 @@
 package com.isai.api_rest_buenas_practicas.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.isai.api_rest_buenas_practicas.models.entity.Category;
 import com.isai.api_rest_buenas_practicas.models.entity.Product;
 
 @Repository
 public interface ProductRepository
         extends JpaRepository<Product, Long> {
 
+    List<Product> findAllByCategory(Category category);
 }
