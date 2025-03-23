907fb651fb6a694715390dc094daf596813c3438
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-23 09:19:57 -0500

    dto para el response de producto, agregacion de la anotacion JsonInclude

--- src/main/java/com/isai/api_rest_buenas_practicas/models/dtos/ProductResponse.java
+++ src/main/java/com/isai/api_rest_buenas_practicas/models/dtos/ProductResponse.java
@@ -2,11 +2,14 @@ package com.isai.api_rest_buenas_practicas.models.dtos;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL) // algun campo sea nulo no se muestra en la respuesta
 public class ProductResponse {
     private Long productId;
     private String name;
