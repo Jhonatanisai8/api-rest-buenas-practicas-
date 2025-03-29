33be6c814263657ff5386db954f3f2e5a34a1f93
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-29 08:55:33 -0500

    ProductService: implementacion del los metodos para buscar un producto por su id, obtener todos los productos, y los productos de una determinada categoria

---
+++ src/main/java/com/isai/api_rest_buenas_practicas/service/implementation/ProductService.java
@@ -0,0 +1,72 @@
+package com.isai.api_rest_buenas_practicas.service.implementation;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.isai.api_rest_buenas_practicas.exceptions.CategoryNotFoundException;
+import com.isai.api_rest_buenas_practicas.exceptions.ProductNotFoundException;
+import com.isai.api_rest_buenas_practicas.mapper.ProductMapper;
+import com.isai.api_rest_buenas_practicas.models.dtos.CategoryResponse;
+import com.isai.api_rest_buenas_practicas.models.dtos.ProductResponse;
+import com.isai.api_rest_buenas_practicas.repository.CategoryRepository;
+import com.isai.api_rest_buenas_practicas.repository.ProductRepository;
+import com.isai.api_rest_buenas_practicas.service.CrudService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService
+        implements CrudService<ProductResponse, CategoryResponse> {
+
+    private final ProductRepository productRepository;
+
+    private final CategoryRepository categoryRepository;
+
+    private final ProductMapper productMapper;
+
+    @Override
+    public ProductResponse findById(Long idBuscar) {
+        return productRepository.findById(idBuscar)
+                .map(product -> productMapper.toProductResponse(product))
+                .orElseThrow(ProductNotFoundException::new);
+    }
+
+    @Override
+    public List<ProductResponse> findAll() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> productMapper.toProductResponse(product))
+                .toList();
+    }
+
+    @Override
+    public List<ProductResponse> findAllByCategoryId(Long idBuscar) {
+        return categoryRepository.findById(idBuscar)
+                .map(category -> productRepository.findAllByCategory(category))
+                .map(products -> products.stream()
+                        .map(produt -> productMapper.toProductResponse(produt))
+                        .toList())
+                .orElseThrow(() -> new CategoryNotFoundException());
+    }
+
+    @Override
+    public ProductResponse save(ProductResponse entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public ProductResponse update(Long id, CategoryResponse request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+}
