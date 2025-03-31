12b5aedd3dad56ff76c71c5eeab5ab6e5b36c193
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-31 10:16:27 -0500

    actualizacion de la interfaz donde estan definidos los metodos crud

--- src/main/java/com/isai/api_rest_buenas_practicas/service/CrudService.java
+++ src/main/java/com/isai/api_rest_buenas_practicas/service/CrudService.java
@@ -2,14 +2,14 @@ package com.isai.api_rest_buenas_practicas.service;
 
 import java.util.List;
 
-public interface CrudService<T, R> {
+public interface CrudService<T, R, Y> {
     T findById(Long idBuscar);
 
     List<T> findAll();
 
     List<T> findAllByCategoryId(Long idBuscar);
 
-    T save(T entity);
+    T save(Y entity);
 
     T update(Long id, R request);
 
