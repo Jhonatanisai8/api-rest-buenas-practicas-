b76e41c2d271d4fe2a8bc3dc3b1adc57bf854455
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-01 14:14:52 -0500

    controlador para productos: implementacion del metodo para guardar un producto

--- src/main/java/com/isai/api_rest_buenas_practicas/controller/ProductController.java
+++ src/main/java/com/isai/api_rest_buenas_practicas/controller/ProductController.java
@@ -3,16 +3,22 @@ package com.isai.api_rest_buenas_practicas.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.isai.api_rest_buenas_practicas.models.dtos.CreateProductRequest;
 import com.isai.api_rest_buenas_practicas.models.dtos.ProductResponse;
 import com.isai.api_rest_buenas_practicas.service.implementation.ProductService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,10 +36,18 @@ public class ProductController {
         return productService.findById(idProduct);
     }
 
-    //lista todos los productos de una categoria
+    // lista todos los productos de una categoria
     @GetMapping("/category/{idCategory}")
     public List<ProductResponse> findAllByCategoryId(@Validated @PathVariable Long idCategory) {
         return productService.findAllByCategoryId(idCategory);
+    }
+
+    @PostMapping("/saveProduct")
+    public ResponseEntity<ProductResponse> saveProductResponse(@Valid @RequestBody CreateProductRequest request) {
+        ProductResponse productResponse = productService.save(request);
+        return ResponseEntity
+                .created(URI.create("/api/products/" + productResponse.getProductId()))
+                .body(productResponse);
     }
 
 }
